d1c36ad0d7055131146fc7bf2294abda31026dce
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2021-08-18 11:55:21 +0300

    update

--- Section 9 - data str, operators, strings/119. Coding Challenge #3/Readmy.md
+++ Section 9 - data str, operators, strings/119. Coding Challenge #3/Readmy.md
@@ -10,13 +10,13 @@ Let's continue with our football betting app! This time, we have a map called 'g
 
 Your tasks:
 
-1. Createanarray'events'ofthedifferentgameeventsthathappened(no duplicates)
+1. Create an array 'events' of the different game events that happened (no duplicates)
 
-2. Afterthegamehasfinished,iswasfoundthattheyellowcardfromminute64 was unfair. So remove this event from the game events log.
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
 
-3. Computeandlogthefollowingstringtotheconsole:"Aneventhappened,on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
 
-4. Loopover'gameEvents'andlogeachelementtotheconsole,marking whether it's in the first half or second half (after 45 min) of the game, like this:
+4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
 [FIRST HALF] 17: ⚽   GOAL
 
 
--- Section 9 - data str, operators, strings/119. Coding Challenge #3/script.js
+++ Section 9 - data str, operators, strings/119. Coding Challenge #3/script.js
@@ -13,3 +13,56 @@ const gameEvents = new Map([
 ]);
 
 
+
+
+// --- TASK 1: Create an array 'events' of the different game events that happened (no duplicates)
+console.log('-------- Task 1 --------');
+
+// 1 шаг - map gameEvents преобразуем в массив только по value -> [...gameEvents.values()]
+// 2 шаг - загоняем массив ([...gameEvents.values()]) в новый Set, который убирает повторные значения, но мы получаем на выходе Set, а нужен массив
+// 3 шаг - при помощи spread конвертируем set в массив
+
+const events = [...new Set([...gameEvents.values()])]
+
+console.log(events);
+
+
+
+
+
+// --- TASK 2: After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+console.log('-------- Task 2 --------');
+
+//  1 шаг - удаление элемента map по ключу
+gameEvents.delete(64);
+console.log(gameEvents);
+
+
+
+
+
+// --- TASK 3: Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+console.log('-------- Task 3 --------');
+
+//  1 шаг - общее время 90 разделил на размер map (то есть на количество событий), чтобы вывести среднее
+
+
+console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`);
+
+
+
+
+
+
+// --- TASK 4: Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this: [FIRST HALF] 17: ⚽   GOAL
+console.log('-------- Task 4 --------');
+
+// 1 шаг - проитерировал мэп, заведя пару [key, value] и потом добавил условие меньше 45 или больше и что выводить в зависисомтси от этого -  key <= 45 ? 'First half' : 'Second half'
+
+for (const [key, value] of gameEvents) {
+  console.log(`${key <= 45 ? 'First half' : 'Second half'} ${key}: ${value}`);
+}
+
+
+
+
